9e71682dc97b87c8b2e7f6af0dd75e12c0ee8417
Jon Chen <bsd@voltaire.sh>
2020-09-22 18:56:08 -0700

    create event bridge

--- lib/sksksk-stack.ts
+++ lib/sksksk-stack.ts
@@ -1,14 +1,17 @@
 import ec2 = require('@aws-cdk/aws-ec2');
 import ecs = require('@aws-cdk/aws-ecs');
+import events = require('@aws-cdk/aws-events');
+import iam = require('@aws-cdk/aws-iam');
 import sns = require('@aws-cdk/aws-sns');
 import path = require('path');
 import * as cdk from '@aws-cdk/core';
+import { eventNames } from 'process';
 
 export class SkskskStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const vpc = new ec2.Vpc(this, 'SkskskVpc', { maxAzs: 3 });
+    const vpc = new ec2.Vpc(this, 'SkskskVpc', { maxAzs: 3 })
 
     new ec2.InterfaceVpcEndpoint(this, 'CloudwatchLogsVpcEndpoint', {
       vpc: vpc,
@@ -34,12 +37,31 @@ export class SkskskStack extends cdk.Stack {
       streamPrefix: "sksksk",
     })
 
-    /*
+    const sepAccount = new iam.AccountPrincipal('006851364659')
+
+    events.EventBus.grantPutEvents(sepAccount)
+
+    const backupCreatedEventBridge = new events.EventBus(this, "SkskskBackupEvent")
+
+    const backupCreatedRule = new events.Rule(this, "SkskskBackupEventRule", {
+      eventBus: backupCreatedEventBridge,
+      enabled: true,
+      eventPattern: {
+        account: ["006851364659"],
+        source: ["aws.s3"],
+        detail:{
+          "eventName": ["PutObject"],
+          "requestParameters": {
+            "bucketName": ["mc.sep.gg-backups"],
+          }
+        }
+      },
+    })
     const cluster = new ecs.Cluster(this, 'SkskskCluster', { vpc });
 
     const taskDef = new ecs.FargateTaskDefinition(this, "SkskskTaskDefinition", {
-      memoryLimitMiB: 4096,
-      cpu: 2048,
+      memoryLimitMiB: 8192,
+      cpu: 4096,
     })
 
     taskDef.addContainer("Spigot", {
@@ -47,6 +69,7 @@ export class SkskskStack extends cdk.Stack {
       logging,
     })
 
+    /*
     new ecs.FargateService(this, "SkskskService", {
       cluster,
       taskDefinition: taskDef
