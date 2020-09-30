975e7c36dc2a64d6f713313a91cf8621204783eb
Jon Chen <szuhchen@twitch.tv>
2020-09-30 05:50:52 +0000

    update s3 sync flags

--- lib/overviewer/wrapper.sh
+++ lib/overviewer/wrapper.sh
@@ -7,4 +7,4 @@ aws s3 cp $BACKUP_S3_URI /data/pumpcraft.tar.gz
 cd /worlds
 tar xvf /data/pumpcraft.tar.gz
 overviewer.py --config /overviewer_cfg.py
-aws s3 sync /output/ s3://skskskstack-skskskworldmapscf6b4e22-1ldpl0tgqx9ok/
+aws s3 sync --storage-class ONEZONE_IA --delete /output/ s3://skskskstack-skskskworldmapscf6b4e22-1ldpl0tgqx9ok/
