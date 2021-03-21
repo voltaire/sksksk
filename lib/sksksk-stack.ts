7ba3cce41b59b95c2ec3d073eeed117e319cb2b7
Jon Chen <bsd@voltaire.sh>
2021-03-21 21:13:02 +0000

    tear down old infra

--- lib/sksksk-stack.ts
+++ lib/sksksk-stack.ts
@@ -9,8 +9,6 @@ export class SkskskStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const sepAccount = new iam.AccountPrincipal('006851364659')
-
     const deployGroup = new iam.Group(this, 'sksksk-deploy', {})
 
     deployGroup.addToPolicy(new iam.PolicyStatement({
@@ -28,54 +26,5 @@ export class SkskskStack extends cdk.Stack {
     const deployUser = new iam.User(this, 'deployUser', {
       groups: [deployGroup],
     })
-
-    const tonkatsuZone = route53.HostedZone.fromHostedZoneAttributes(this, 'tonkatsuZone', {
-      hostedZoneId: 'ZVAMW53PNR70P',
-      zoneName: 'tonkat.su',
-    })
-
-    const sepBucket = s3.Bucket.fromBucketName(this, 'sepBucket', 'mc.sep.gg-backups')
-    sepBucket.grantRead(deployGroup)
-
-    const backupNotificationTopic = new sns.Topic(this, "SkskskBackupTopic", {});
-    backupNotificationTopic.grantPublish(sepAccount)
-    backupNotificationTopic.addToResourcePolicy(new iam.PolicyStatement({
-      actions: ["sns:Publish"],
-      resources: [backupNotificationTopic.topicArn],
-      principals: [new iam.ServicePrincipal("s3.amazonaws.com")],
-      conditions: {
-        ArnEquals: {"aws:SourceArn": sepBucket.bucketArn}
-      },
-    }))
-    deployGroup.addToPolicy(new iam.PolicyStatement({
-      actions: ["sns:Subscribe", "sns:ConfirmSubscription"],
-      resources: [backupNotificationTopic.topicArn],
-    }))
-
-    new route53.CnameRecord(this, 'mapCname', {
-      zone: tonkatsuZone,
-      recordName: 'map',
-      domainName: 'map.tonkat.su.website-us-east-1.linodeobjects.com',
-    })
-
-    new route53.CnameRecord(this, 'oldmapCname', {
-      zone: tonkatsuZone,
-      recordName: 'oldmap',
-      domainName: 'oldmap.tonkat.su.website-us-east-1.linodeobjects.com',
-    })
-
-    new route53.CnameRecord(this, 'bungeeCord', {
-      zone: tonkatsuZone,
-      recordName: 'mc',
-      domainName: 'mc.sep.gg',
-    })
-
-    const rendererRecord = new route53.ARecord(this, 'renderer', {
-      zone: tonkatsuZone,
-      recordName: 'renderer',
-      target: route53.RecordTarget.fromIpAddresses('107.150.36.10'),
-    })
-
-    backupNotificationTopic.addSubscription(new subscriptions.UrlSubscription('https://'+rendererRecord.domainName+'/callback', {protocol: sns.SubscriptionProtocol.HTTPS}))
   }
 }
